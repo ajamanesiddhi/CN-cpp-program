fa634a0c0008619bd9204920b2681ef6b647ad88
ajamanesiddhi <ajamanesiddhi@gmail.com>
2026-08-21 04:40:51 +0000

    change

--- stopandwait.cpp
+++ stopandwait.cpp
@@ -1,21 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
+
 int main()
 {
     int n;
-    cout<<"Enter number of frames:";
-    cin>>n;
-    for(int i=1;i<=n;i++)
+
+    cout << "Enter number of frames: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
     {
-        cout<<"\nSender:Sending frame"
-        <<i;
-        cout<<"\nReceiver:frame"<<i<<
-        "received";
-        cout<<"\nReceiver:Sending ACK"
-        <<i;
-        cout<<"\nSender:ACK"<<i<<
-        "received";
+        cout << "\nSender: Sending Frame " << i;
+        this_thread::sleep_for(chrono::milliseconds(100));
+
+        cout << "\nReceiver: Frame " << i << " received";
+        this_thread::sleep_for(chrono::milliseconds(100));
+
+        cout << "\nReceiver: Sending ACK " << i;
+        this_thread::sleep_for(chrono::milliseconds(100));
+
+        cout << "\nSender: ACK " << i << " received";
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
-    cout<<"\n\nAll frames transmitted successfully.";
+
+    cout << "\n\nAll frames transmitted successfully.";
+
     return 0;
 }
